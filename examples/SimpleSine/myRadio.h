c63a8edee8b0a0f85e3507d3a9d69ddf5e4d37f4
TMRh20 <tmrh20@gmail.com>
2016-11-11 07:29:02 -0600

    Add SimpleSine example
    Add SimpleSine example:
    - Example of sine wave generation and optionally broadcast via radio
    with local volume control and 2 frequencies/tones

---
+++ examples/SimpleSine/myRadio.h
@@ -0,0 +1,40 @@
+
+
+#include <RF24.h>
+
+RF24 radio(46,52);
+
+const uint64_t pipes[14] = { 0xABCDABCD71LL, 0x544d52687CLL, 0x544d526832LL };
+
+bool tx,fail,rx;
+uint8_t pipeNo = 0;
+
+void RX();
+
+void setupRadio(){
+  
+  radio.begin();
+  radio.setChannel(1);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setDataRate(RF24_1MBPS);
+  radio.setAutoAck(0);
+  radio.setCRCLength(RF24_CRC_8);
+  radio.setAddressWidth(5);
+  radio.openReadingPipe(1,pipes[1]);
+  radio.openReadingPipe(2,pipes[2]);
+  radio.setAutoAck(2,1);
+  radio.openWritingPipe(pipes[0]);
+  radio.txDelay = 0;
+  radio.csDelay = 0;
+  radio.maskIRQ(0,1,0);
+  radio.printDetails();
+
+  //radio.startListening();
+  radio.stopListening();
+  //attachInterrupt(digitalPinToInterrupt(4),RX,FALLING);
+  
+}
+
+
+
+
