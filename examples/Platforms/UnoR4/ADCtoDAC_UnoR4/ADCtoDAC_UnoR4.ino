71288e0b244393929c78f84ffa38bcd08c7ddf9f
TMRh20 <tmrh20@gmail.com>
2026-08-04 07:12:08 -0600

    Update ADCtoDAC_UnoR4.ino

--- examples/Platforms/UnoR4/ADCtoDAC_UnoR4/ADCtoDAC_UnoR4.ino
+++ examples/Platforms/UnoR4/ADCtoDAC_UnoR4/ADCtoDAC_UnoR4.ino
@@ -28,9 +28,9 @@ void loop() {
   aaAudio.getADC(bufferSize);
 
   // Copy our 14-bit ADC data to the DAC buffer which is only 12-bit
-  // The dac input expects signed 12-bit samples
+  // The dac input expects signed 16-bit samples
   for(int i=0; i<bufferSize; i++){
-    aaAudio.dacBuffer16[i] = (aaAudio.adcBuffer16[i] ^ 0x8000) >> 2;  
+    aaAudio.dacBuffer16[i] = (aaAudio.adcBuffer16[i] ^ 0x8000);  
   }
   aaAudio.feedDAC(0,bufferSize);
 }
