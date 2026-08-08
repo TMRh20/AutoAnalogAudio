1c5fbdd259e53dcd7d9507e1029669658235e3d2
TMRh20 <tmrh20@gmail.com>
2026-08-08 17:06:28 -0600

    Add WiFi streaming example for R4
    - Add WiFi streaming example for R4
    - Update library.properties and json for R4 devices
    - Adjust buffer size to 512 from 256 for R4 devices

--- src/AutoAnalog_config.h
+++ src/AutoAnalog_config.h
@@ -24,7 +24,7 @@
   
   #elif defined (ARDUINO_ARCH_RENESAS)
     
-    #define MAX_BUFFER_SIZE 256
+    #define MAX_BUFFER_SIZE 512
   
   #elif defined ESP32 || defined ARDUINO_ARCH_NRF52840 || defined (ARDUINO_ARCH_NRF52) || defined ARDUINO_NRF52840_FEATHER || defined ARDUINO_NRF54L15
       
